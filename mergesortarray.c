b95309b66e82ed95e83d09633f33102719157837
aswin_pr <97214080+aswinpr@users.noreply.github.com>
2023-10-17 09:45:57 +0530

    mergesortarray.c

--- mergesortarray.c
+++ mergesortarray.c
@@ -2,9 +2,10 @@
 void main(){
 
 	int arr1[50],arr2[50],limit,i,arr[50],temp,j;
+
 	printf("enter the limit of array: ");
 	scanf("%d",&limit);
-
+	int arrlim=limit*2;
 	printf("enter the elemets of array 1:");
 	for(i=0;i<limit;i++){
 		scanf("%d",&arr1[i]);
@@ -14,28 +15,45 @@ void main(){
 		scanf("%d",&arr2[i]);
 
 	}
-/*	//print elements of array
- *
+	//print elements of array
+
 	printf("The elements of array 1: \t");
 	for(i=0;i<limit;i++){
-		printf("%d",arr1[i]);
+		printf("%d \t",arr1[i]);
 	}
-	printf("The elements of array 3: \t");
+	printf("\n");
+	printf("The elements of array 2: \t");
 	for(i=0;i<limit;i++){
-		printf("%d",arr2[i]);
+		printf("%d \t",arr2[i]);
 	}
-*/
+
+	//merging
+	for(i=0;i<limit;i++){
+		arr[i]=arr1[i];
+	}
+	for(i=0;i<limit;i++){
+		arr[limit+i]=arr2[i];
+	}
+
 
 	//sort main array
-
-
-
-	//print sorted array
-
-	printf("The elements of arr: \t");
-		for(i=0;i<limit;i++){
-			printf("%d \t",arr[i]);
+	for(i=0;i<arrlim;i++){
+		for(j=i+1;j<arrlim;j++){
+			if(arr[i]>arr[j]){
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
 		}
+	}
+
+    printf("\n");
+	//print sorted array
+    
+	printf("The elements after merged and sorted : \t");
+	for(i=0;i<arrlim;i++){
+		printf("%d \t",arr[i]);
+	}
 
 }
 
