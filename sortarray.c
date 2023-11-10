26ced7987404e5df7382ef3eb29b36b5c0492435
aswin_pr <97214080+aswinpr@users.noreply.github.com>
2023-11-10 16:45:00 +0530

    mergesortarray.c

--- sortarray.c
+++ sortarray.c
@@ -1,59 +1,55 @@
-#include <stdio.h>
-void main(){
-
-	int arr1[50],arr2[50],limit,i,arr[50],temp,j;
-
-	printf("enter the limit of array: ");
-	scanf("%d",&limit);
-	int arrlim=limit*2;
-	printf("enter the elemets of array 1:");
-	for(i=0;i<limit;i++){
-		scanf("%d",&arr1[i]);
-	}
-	printf("enter the elements of array 2:");
-	for(i=0;i<limit;i++){
-		scanf("%d",&arr2[i]);
-
-	}
-	//print elements of array
-
-	printf("The elements of array 1: \t");
-	for(i=0;i<limit;i++){
-		printf("%d \t",arr1[i]);
-	}
-	printf("\n");
-	printf("The elements of array 2: \t");
-	for(i=0;i<limit;i++){
-		printf("%d \t",arr2[i]);
-	}
-
-	//merging
-	for(i=0;i<limit;i++){
-		arr[i]=arr1[i];
-	}
-	for(i=0;i<limit;i++){
-		arr[limit+i]=arr2[i];
-	}
-
-
-	//sort main array
-	for(i=0;i<arrlim;i++){
-		for(j=i+1;j<arrlim;j++){
-			if(arr[i]>arr[j]){
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-			}
-		}
-	}
-
-    printf("\n");
-	//print sorted array
-    
-	printf("The elements after merged and sorted : \t");
-	for(i=0;i<arrlim;i++){
-		printf("%d \t",arr[i]);
-	}
-
+#include<stdio.h>
+void main()
+{
+  int m,n,k,d,temp,i,j;
+  printf("Enter the size of first array:");
+  scanf("%d",&m);
+  printf("Enter the size of second array:");
+  scanf("%d",&n);
+   d=m+n;
+  int a[m],b[n],c[d];
+  printf("Enter the elements of first sorted array:");
+  for( i=0;i<m;i++)
+  {
+    scanf("%d",&a[i]);
+   
+   }
+  printf("Enter the elements of second sorted array:");
+  for( j=0;j<n;j++)
+  {
+    scanf("%d",&b[j]);
+  }
+  k=0;i=0;j=0;
+  while((i<m)&&(i<n))
+     {
+       if(a[i]>b[j])
+       {
+         c[k]=b[j];
+         k++;
+         j++;
+        }
+        else
+        {
+          c[k]=a[i];
+          k++;
+          i++;
+         }
+      }
+   while(i<m)
+   {
+     c[k]=a[i];
+     i++;
+     k++;
+    }
+   while(j<n)
+   {
+     c[k]=b[j];
+      j++;
+      k++;
+   }
+printf("\nThe sorted merged array is:");
+for(k=0;k<d;k++)
+{
+  printf("%d\t",c[k]);
 }
-
+}
